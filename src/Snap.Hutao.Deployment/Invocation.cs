a16fcbd2bb8269e2d9ddf1aaf77fa71647b278d1
qhy040404 <qhy040404@163.com>
2023-12-28 19:34:04 +0800

    fix ERROR_NOT_FOUND

--- src/Snap.Hutao.Deployment/Invocation.cs
+++ src/Snap.Hutao.Deployment/Invocation.cs
@@ -92,8 +92,8 @@ internal static partial class Invocation
                         }
                         catch (COMException ex)
                         {
-                            // ERROR_MRM_MAP_NOT_FOUND
-                            if (ex.HResult is not unchecked((int)0x80073B1F))
+                            // ERROR_MRM_MAP_NOT_FOUND or ERROR_NOT_FOUND
+                            if (ex.HResult is not unchecked((int)0x80073B1F) or unchecked((int)0x80070490))
                             {
                                 throw;
                             }
