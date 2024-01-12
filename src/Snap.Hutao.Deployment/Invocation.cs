d6f5b7374d3f11251303cf890f65f8a70c0641be
qhy040404 <qhy040404@163.com>
2024-01-12 09:13:13 +0800

    global try and enter to exit

--- src/Snap.Hutao.Deployment/Invocation.cs
+++ src/Snap.Hutao.Deployment/Invocation.cs
@@ -45,77 +45,11 @@ internal static partial class Invocation
             }
         }
 
-        await Certificate.EnsureGlobalSignCodeSigningRootR45Async().ConfigureAwait(false);
-        await WindowsAppSDKDependency.EnsureAsync(path).ConfigureAwait(false);
-        await RunDeploymentCoreAsync(path, name, isUpdateMode).ConfigureAwait(false);
-        await ExitAsync(isUpdateMode).ConfigureAwait(false);
-    }
-
-    private static async Task RunDeploymentCoreAsync(string path, string? name, bool isUpdateMode)
-    {
         try
         {
-            Console.WriteLine("Initializing PackageManager...");
-            PackageManager packageManager = new();
-            AddPackageOptions addPackageOptions = new()
-            {
-                ForceAppShutdown = true,
-                RetainFilesOnFailure = true,
-            };
-
-            Console.WriteLine("Start deploying...");
-            IProgress<DeploymentProgress> progress = new Progress<DeploymentProgress>(p =>
-            {
-                Console.WriteLine($"[Deploying]: State: {p.state} Progress: {p.percentage}%");
-            });
-            DeploymentResult result = await packageManager
-                .AddPackageByUriAsync(new Uri(path), addPackageOptions)
-                .AsTask(progress)
-                .ConfigureAwait(false);
-
-            if (result.IsRegistered)
-            {
-                Console.WriteLine("Package deployed.");
-                if (string.IsNullOrEmpty(name))
-                {
-                    Console.WriteLine("FamilyName not provided, enumerating packages.");
-
-                    foreach (Windows.ApplicationModel.Package package in packageManager.FindPackages())
-                    {
-                        try
-                        {
-                            if (package is { DisplayName: "Snap Hutao", PublisherDisplayName: "DGP Studio" })
-                            {
-                                name = package.Id.FamilyName;
-                                Console.WriteLine($"Package found: {name}");
-                            }
-                        }
-                        catch (COMException ex)
-                        {
-                            // ERROR_MRM_MAP_NOT_FOUND or ERROR_NOT_FOUND
-                            if (ex.HResult is not unchecked((int)0x80073B1F) or unchecked((int)0x80070490))
-                            {
-                                throw;
-                            }
-                        }
-                    }
-                }
-
-                Console.WriteLine("Starting app...");
-                Process.Start(new ProcessStartInfo()
-                {
-                    UseShellExecute = true,
-                    FileName = $@"shell:AppsFolder\{name}!App",
-                });
-            }
-            else
-            {
-                Console.WriteLine($"""
-                    ActivityId: {result.ActivityId}
-                    ExtendedErrorCode: {result.ExtendedErrorCode}
-                    ErrorText: {result.ErrorText}
-                    """);
-            }
+            await Certificate.EnsureGlobalSignCodeSigningRootR45Async().ConfigureAwait(false);
+            await WindowsAppSDKDependency.EnsureAsync(path).ConfigureAwait(false);
+            await RunDeploymentCoreAsync(path, name, isUpdateMode).ConfigureAwait(false);
         }
         catch (Exception ex)
         {
@@ -124,14 +58,83 @@ internal static partial class Invocation
                 {ex}
                 """);
         }
+        finally
+        {
+            await ExitAsync(isUpdateMode).ConfigureAwait(false);
+        }
+    }
+
+    private static async Task RunDeploymentCoreAsync(string path, string? name, bool isUpdateMode)
+    {
+        Console.WriteLine("Initializing PackageManager...");
+        PackageManager packageManager = new();
+        AddPackageOptions addPackageOptions = new()
+        {
+            ForceAppShutdown = true,
+            RetainFilesOnFailure = true,
+        };
+
+        Console.WriteLine("Start deploying...");
+        IProgress<DeploymentProgress> progress = new Progress<DeploymentProgress>(p =>
+        {
+            Console.WriteLine($"[Deploying]: State: {p.state} Progress: {p.percentage}%");
+        });
+        DeploymentResult result = await packageManager
+            .AddPackageByUriAsync(new Uri(path), addPackageOptions)
+            .AsTask(progress)
+            .ConfigureAwait(false);
+
+        if (result.IsRegistered)
+        {
+            Console.WriteLine("Package deployed.");
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("FamilyName not provided, enumerating packages.");
+
+                foreach (Windows.ApplicationModel.Package package in packageManager.FindPackages())
+                {
+                    try
+                    {
+                        if (package is { DisplayName: "Snap Hutao", PublisherDisplayName: "DGP Studio" })
+                        {
+                            name = package.Id.FamilyName;
+                            Console.WriteLine($"Package found: {name}");
+                        }
+                    }
+                    catch (COMException ex)
+                    {
+                        // ERROR_MRM_MAP_NOT_FOUND or ERROR_NOT_FOUND
+                        if (ex.HResult is not unchecked((int)0x80073B1F) or unchecked((int)0x80070490))
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Starting app...");
+            Process.Start(new ProcessStartInfo()
+            {
+                UseShellExecute = true,
+                FileName = $@"shell:AppsFolder\{name}!App",
+            });
+        }
+        else
+        {
+            Console.WriteLine($"""
+                    ActivityId: {result.ActivityId}
+                    ExtendedErrorCode: {result.ExtendedErrorCode}
+                    ErrorText: {result.ErrorText}
+                    """);
+        }
     }
 
     private static async ValueTask ExitAsync(bool isUpdateMode)
     {
         if (!isUpdateMode)
         {
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            Console.WriteLine("Press enter to exit...");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter) ;
             FreeConsole();
         }
         else
